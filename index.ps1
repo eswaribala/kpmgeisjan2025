65e758f9d064e097c3b696ec066300a32204931d
eswaribala <Parameswaribala@gmail.com>
2025-01-30 12:00:27 +0530

    Create index.ps1

---
+++ index.ps1
@@ -0,0 +1,18 @@
+$processName = "notepad"
+
+#check notepad opened or not
+
+$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+
+if ($process) {
+
+  Write-Output "Process '$processName' is running"
+  Stop-Process -Name $processName --Force
+
+
+} else {
+
+  Write-Output "Process '$processName' is not running"
+
+
+}
